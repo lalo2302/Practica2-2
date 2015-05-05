313ead4bec738e225a0b94d22060c9cc2d7ef75a
Eduardo Aguilera Olascoaga <laloao.2302@gmail.com>
2015-05-04 23:47:36 -0500

    Se agrego DLL de base de datos y clases para su consumo

---
+++ Practica2-2/Practica2-2/Clases/Estructura.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica2_2.Clases
+{
+    public class Estructura
+    {
+        SqlParameter[] parametros;
+
+        public SqlParameter[] Parametros
+        {
+            get { return parametros; }
+            set { parametros = value; }
+        }
+
+        List<object> valores;
+
+        public List<object> Valores
+        {
+            get { return valores; }
+            set { valores = value; }
+        }
+
+        string sentencia;
+
+        public string Sentencia
+        {
+            get { return sentencia; }
+            set { sentencia = value; }
+        }
+
+        string tabla;
+
+        public string Tabla
+        {
+            get { return tabla; }
+            set { tabla = value; }
+        }
+
+        public Estructura()
+        {
+            valores = new List<object>();
+        }
+    }
+}
---
+++ Practica2-2/Practica2-2/Clases/Operaciones.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseDatos;
+using BaseDatos.ConexionBD;
+using System.Data;
+
+namespace Practica2_2.Clases
+{
+    class Operaciones
+    {
+        private Estructura elemento;
+
+        public Estructura Elemento
+        {
+            set { elemento = value; }
+            get { return elemento; }
+        }
+
+        private Ejecucion objDatos;
+
+        public Operaciones()
+        {
+            if (objDatos == null)
+            {
+                string cadena = System.Configuration.ConfigurationManager.ConnectionStrings["CadenaSqlSrv"].ConnectionString;
+
+                objDatos = new Ejecucion(cadena);
+            }
+        }
+
+        public bool Ejecutar()
+        {
+            bool valido = false;
+            int n = objDatos.EjecutaComando(elemento.Parametros, elemento.Valores, elemento.Sentencia, CommandType.Text);
+            if (n > 0)
+            {
+                valido = true;
+            }
+            return valido;
+        }
+
+        public DataTable ObtenerCredenciales()
+        {
+            DataTable datos = new DataTable();
+            datos = objDatos.SeleccionaDatosDesconectado(elemento.Parametros, elemento.Valores, elemento.Sentencia, CommandType.Text, "tbl_Credencial");
+            return datos;
+        }
+
+        public int ConfirmarInicio()
+        {
+            int n = objDatos.EjecutaScalar(elemento.Parametros, elemento.Valores, elemento.Sentencia, CommandType.Text);
+
+            return n;
+        }
+    }
+}
