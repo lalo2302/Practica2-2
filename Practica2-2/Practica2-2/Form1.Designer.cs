5b39e7008df78a0681da47ca01ba233200636a74
Eduardo Aguilera Olascoaga <laloao.2302@gmail.com>
2015-05-05 01:40:37 -0500

    Cambio de estatica en groupbox de texto y enteros con sus respectivos botones

--- Practica2-2/Practica2-2/Form1.Designer.cs
+++ Practica2-2/Practica2-2/Form1.Designer.cs
@@ -74,7 +74,10 @@
             this.lblBd = new System.Windows.Forms.Label();
             this.txtBd = new System.Windows.Forms.TextBox();
             this.btnBd = new System.Windows.Forms.Button();
-            this.btnFiltros = new System.Windows.Forms.Button();
+            this.btnApFiltroS = new System.Windows.Forms.Button();
+            this.btnNingunoS = new System.Windows.Forms.Button();
+            this.btnNingunoI = new System.Windows.Forms.Button();
+            this.btnApFiltroI = new System.Windows.Forms.Button();
             this.gboxInt.SuspendLayout();
             this.gboxString.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRes)).BeginInit();
@@ -118,6 +121,8 @@
             // 
             // gboxInt
             // 
+            this.gboxInt.Controls.Add(this.btnApFiltroI);
+            this.gboxInt.Controls.Add(this.btnNingunoI);
             this.gboxInt.Controls.Add(this.chDif);
             this.gboxInt.Controls.Add(this.chIgual);
             this.gboxInt.Controls.Add(this.chMenor);
@@ -134,9 +139,9 @@
             this.gboxInt.Controls.Add(this.txtMayor);
             this.gboxInt.Controls.Add(this.lblCampo2);
             this.gboxInt.Controls.Add(this.lblCampo1);
-            this.gboxInt.Location = new System.Drawing.Point(230, 66);
+            this.gboxInt.Location = new System.Drawing.Point(230, 50);
             this.gboxInt.Name = "gboxInt";
-            this.gboxInt.Size = new System.Drawing.Size(174, 121);
+            this.gboxInt.Size = new System.Drawing.Size(174, 164);
             this.gboxInt.TabIndex = 5;
             this.gboxInt.TabStop = false;
             this.gboxInt.Text = "Número";
@@ -144,7 +149,7 @@
             // chDif
             // 
             this.chDif.AutoSize = true;
-            this.chDif.Location = new System.Drawing.Point(108, 95);
+            this.chDif.Location = new System.Drawing.Point(111, 115);
             this.chDif.Name = "chDif";
             this.chDif.Size = new System.Drawing.Size(15, 14);
             this.chDif.TabIndex = 17;
@@ -153,7 +158,7 @@
             // chIgual
             // 
             this.chIgual.AutoSize = true;
-            this.chIgual.Location = new System.Drawing.Point(108, 69);
+            this.chIgual.Location = new System.Drawing.Point(111, 89);
             this.chIgual.Name = "chIgual";
             this.chIgual.Size = new System.Drawing.Size(15, 14);
             this.chIgual.TabIndex = 16;
@@ -162,7 +167,7 @@
             // chMenor
             // 
             this.chMenor.AutoSize = true;
-            this.chMenor.Location = new System.Drawing.Point(108, 42);
+            this.chMenor.Location = new System.Drawing.Point(111, 62);
             this.chMenor.Name = "chMenor";
             this.chMenor.Size = new System.Drawing.Size(15, 14);
             this.chMenor.TabIndex = 15;
@@ -171,7 +176,7 @@
             // chMayor
             // 
             this.chMayor.AutoSize = true;
-            this.chMayor.Location = new System.Drawing.Point(108, 16);
+            this.chMayor.Location = new System.Drawing.Point(111, 36);
             this.chMayor.Name = "chMayor";
             this.chMayor.Size = new System.Drawing.Size(15, 14);
             this.chMayor.TabIndex = 14;
@@ -180,7 +185,7 @@
             // lblDif
             // 
             this.lblDif.AutoSize = true;
-            this.lblDif.Location = new System.Drawing.Point(52, 94);
+            this.lblDif.Location = new System.Drawing.Point(55, 114);
             this.lblDif.Name = "lblDif";
             this.lblDif.Size = new System.Drawing.Size(16, 13);
             this.lblDif.TabIndex = 13;
@@ -189,7 +194,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 94);
+            this.label2.Location = new System.Drawing.Point(6, 114);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 13);
             this.label2.TabIndex = 12;
@@ -198,7 +203,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 68);
+            this.label1.Location = new System.Drawing.Point(6, 88);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 13);
             this.label1.TabIndex = 11;
@@ -207,7 +212,7 @@
             // lblIgual
             // 
             this.lblIgual.AutoSize = true;
-            this.lblIgual.Location = new System.Drawing.Point(55, 68);
+            this.lblIgual.Location = new System.Drawing.Point(58, 88);
             this.lblIgual.Name = "lblIgual";
             this.lblIgual.Size = new System.Drawing.Size(13, 13);
             this.lblIgual.TabIndex = 10;
@@ -215,21 +220,21 @@
             // 
             // txtDif
             // 
-            this.txtDif.Location = new System.Drawing.Point(74, 91);
+            this.txtDif.Location = new System.Drawing.Point(77, 111);
             this.txtDif.Name = "txtDif";
             this.txtDif.Size = new System.Drawing.Size(28, 20);
             this.txtDif.TabIndex = 9;
             // 
             // txtIgual
             // 
-            this.txtIgual.Location = new System.Drawing.Point(74, 65);
+            this.txtIgual.Location = new System.Drawing.Point(77, 85);
             this.txtIgual.Name = "txtIgual";
             this.txtIgual.Size = new System.Drawing.Size(28, 20);
             this.txtIgual.TabIndex = 8;
             // 
             // txtMenor
             // 
-            this.txtMenor.Location = new System.Drawing.Point(74, 39);
+            this.txtMenor.Location = new System.Drawing.Point(77, 59);
             this.txtMenor.Name = "txtMenor";
             this.txtMenor.Size = new System.Drawing.Size(28, 20);
             this.txtMenor.TabIndex = 7;
@@ -237,7 +242,7 @@
             // lblMenor
             // 
             this.lblMenor.AutoSize = true;
-            this.lblMenor.Location = new System.Drawing.Point(55, 42);
+            this.lblMenor.Location = new System.Drawing.Point(58, 62);
             this.lblMenor.Name = "lblMenor";
             this.lblMenor.Size = new System.Drawing.Size(13, 13);
             this.lblMenor.TabIndex = 6;
@@ -246,7 +251,7 @@
             // lblMayor
             // 
             this.lblMayor.AutoSize = true;
-            this.lblMayor.Location = new System.Drawing.Point(55, 16);
+            this.lblMayor.Location = new System.Drawing.Point(58, 36);
             this.lblMayor.Name = "lblMayor";
             this.lblMayor.Size = new System.Drawing.Size(13, 13);
             this.lblMayor.TabIndex = 5;
@@ -254,7 +259,7 @@
             // 
             // txtMayor
             // 
-            this.txtMayor.Location = new System.Drawing.Point(74, 13);
+            this.txtMayor.Location = new System.Drawing.Point(77, 33);
             this.txtMayor.Name = "txtMayor";
             this.txtMayor.Size = new System.Drawing.Size(28, 20);
             this.txtMayor.TabIndex = 4;
@@ -262,7 +267,7 @@
             // lblCampo2
             // 
             this.lblCampo2.AutoSize = true;
-            this.lblCampo2.Location = new System.Drawing.Point(3, 42);
+            this.lblCampo2.Location = new System.Drawing.Point(6, 62);
             this.lblCampo2.Name = "lblCampo2";
             this.lblCampo2.Size = new System.Drawing.Size(46, 13);
             this.lblCampo2.TabIndex = 2;
@@ -271,7 +276,7 @@
             // lblCampo1
             // 
             this.lblCampo1.AutoSize = true;
-            this.lblCampo1.Location = new System.Drawing.Point(3, 16);
+            this.lblCampo1.Location = new System.Drawing.Point(6, 36);
             this.lblCampo1.Name = "lblCampo1";
             this.lblCampo1.Size = new System.Drawing.Size(46, 13);
             this.lblCampo1.TabIndex = 0;
@@ -287,6 +292,8 @@
             // 
             // gboxString
             // 
+            this.gboxString.Controls.Add(this.btnApFiltroS);
+            this.gboxString.Controls.Add(this.btnNingunoS);
             this.gboxString.Controls.Add(this.checkBox1);
             this.gboxString.Controls.Add(this.checkBox2);
             this.gboxString.Controls.Add(this.checkBox3);
@@ -303,9 +310,9 @@
             this.gboxString.Controls.Add(this.txtCamp1);
             this.gboxString.Controls.Add(this.lblCamp2);
             this.gboxString.Controls.Add(this.lblCamp1);
-            this.gboxString.Location = new System.Drawing.Point(230, 66);
+            this.gboxString.Location = new System.Drawing.Point(230, 50);
             this.gboxString.Name = "gboxString";
-            this.gboxString.Size = new System.Drawing.Size(174, 121);
+            this.gboxString.Size = new System.Drawing.Size(174, 166);
             this.gboxString.TabIndex = 18;
             this.gboxString.TabStop = false;
             this.gboxString.Text = "Texto";
@@ -313,7 +320,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(149, 95);
+            this.checkBox1.Location = new System.Drawing.Point(153, 117);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(15, 14);
             this.checkBox1.TabIndex = 17;
@@ -322,7 +329,7 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(149, 69);
+            this.checkBox2.Location = new System.Drawing.Point(153, 91);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(15, 14);
             this.checkBox2.TabIndex = 16;
@@ -331,7 +338,7 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(149, 42);
+            this.checkBox3.Location = new System.Drawing.Point(153, 64);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(15, 14);
             this.checkBox3.TabIndex = 15;
@@ -340,7 +347,7 @@
             // checkBox4
             // 
             this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(149, 16);
+            this.checkBox4.Location = new System.Drawing.Point(153, 38);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(15, 14);
             this.checkBox4.TabIndex = 14;
@@ -349,7 +356,7 @@
             // lblIgualS
             // 
             this.lblIgualS.AutoSize = true;
-            this.lblIgualS.Location = new System.Drawing.Point(62, 95);
+            this.lblIgualS.Location = new System.Drawing.Point(66, 117);
             this.lblIgualS.Name = "lblIgualS";
             this.lblIgualS.Size = new System.Drawing.Size(13, 13);
             this.lblIgualS.TabIndex = 13;
@@ -358,7 +365,7 @@
             // lblCamp4
             // 
             this.lblCamp4.AutoSize = true;
-            this.lblCamp4.Location = new System.Drawing.Point(3, 94);
+            this.lblCamp4.Location = new System.Drawing.Point(7, 116);
             this.lblCamp4.Name = "lblCamp4";
             this.lblCamp4.Size = new System.Drawing.Size(46, 13);
             this.lblCamp4.TabIndex = 12;
@@ -367,7 +374,7 @@
             // lblCamp3
             // 
             this.lblCamp3.AutoSize = true;
-            this.lblCamp3.Location = new System.Drawing.Point(3, 68);
+            this.lblCamp3.Location = new System.Drawing.Point(7, 90);
             this.lblCamp3.Name = "lblCamp3";
             this.lblCamp3.Size = new System.Drawing.Size(46, 13);
             this.lblCamp3.TabIndex = 11;
@@ -376,7 +383,7 @@
             // lblCont
             // 
             this.lblCont.AutoSize = true;
-            this.lblCont.Location = new System.Drawing.Point(55, 68);
+            this.lblCont.Location = new System.Drawing.Point(59, 90);
             this.lblCont.Name = "lblCont";
             this.lblCont.Size = new System.Drawing.Size(28, 13);
             this.lblCont.TabIndex = 10;
@@ -384,21 +391,21 @@
             // 
             // txtCamp4
             // 
-            this.txtCamp4.Location = new System.Drawing.Point(91, 91);
+            this.txtCamp4.Location = new System.Drawing.Point(95, 113);
             this.txtCamp4.Name = "txtCamp4";
             this.txtCamp4.Size = new System.Drawing.Size(52, 20);
             this.txtCamp4.TabIndex = 9;
             // 
             // txtCamp3
             // 
-            this.txtCamp3.Location = new System.Drawing.Point(91, 65);
+            this.txtCamp3.Location = new System.Drawing.Point(95, 87);
             this.txtCamp3.Name = "txtCamp3";
             this.txtCamp3.Size = new System.Drawing.Size(52, 20);
             this.txtCamp3.TabIndex = 8;
             // 
             // txtCamp2
             // 
-            this.txtCamp2.Location = new System.Drawing.Point(91, 39);
+            this.txtCamp2.Location = new System.Drawing.Point(95, 61);
             this.txtCamp2.Name = "txtCamp2";
             this.txtCamp2.Size = new System.Drawing.Size(52, 20);
             this.txtCamp2.TabIndex = 7;
@@ -406,7 +413,7 @@
             // lblTer
             // 
             this.lblTer.AutoSize = true;
-            this.lblTer.Location = new System.Drawing.Point(58, 42);
+            this.lblTer.Location = new System.Drawing.Point(62, 64);
             this.lblTer.Name = "lblTer";
             this.lblTer.Size = new System.Drawing.Size(20, 13);
             this.lblTer.TabIndex = 6;
@@ -415,7 +422,7 @@
             // lblEmp
             // 
             this.lblEmp.AutoSize = true;
-            this.lblEmp.Location = new System.Drawing.Point(60, 16);
+            this.lblEmp.Location = new System.Drawing.Point(64, 38);
             this.lblEmp.Name = "lblEmp";
             this.lblEmp.Size = new System.Drawing.Size(20, 13);
             this.lblEmp.TabIndex = 5;
@@ -423,7 +430,7 @@
             // 
             // txtCamp1
             // 
-            this.txtCamp1.Location = new System.Drawing.Point(91, 13);
+            this.txtCamp1.Location = new System.Drawing.Point(95, 35);
             this.txtCamp1.Name = "txtCamp1";
             this.txtCamp1.Size = new System.Drawing.Size(52, 20);
             this.txtCamp1.TabIndex = 4;
@@ -431,7 +438,7 @@
             // lblCamp2
             // 
             this.lblCamp2.AutoSize = true;
-            this.lblCamp2.Location = new System.Drawing.Point(3, 42);
+            this.lblCamp2.Location = new System.Drawing.Point(7, 64);
             this.lblCamp2.Name = "lblCamp2";
             this.lblCamp2.Size = new System.Drawing.Size(46, 13);
             this.lblCamp2.TabIndex = 2;
@@ -440,7 +447,7 @@
             // lblCamp1
             // 
             this.lblCamp1.AutoSize = true;
-            this.lblCamp1.Location = new System.Drawing.Point(3, 16);
+            this.lblCamp1.Location = new System.Drawing.Point(7, 38);
             this.lblCamp1.Name = "lblCamp1";
             this.lblCamp1.Size = new System.Drawing.Size(46, 13);
             this.lblCamp1.TabIndex = 0;
@@ -449,7 +456,7 @@
             // lblQuery
             // 
             this.lblQuery.AutoSize = true;
-            this.lblQuery.Location = new System.Drawing.Point(12, 200);
+            this.lblQuery.Location = new System.Drawing.Point(12, 227);
             this.lblQuery.Name = "lblQuery";
             this.lblQuery.Size = new System.Drawing.Size(35, 13);
             this.lblQuery.TabIndex = 19;
@@ -460,7 +467,7 @@
             this.dgvRes.AllowUserToAddRows = false;
             this.dgvRes.AllowUserToDeleteRows = false;
             this.dgvRes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRes.Location = new System.Drawing.Point(12, 228);
+            this.dgvRes.Location = new System.Drawing.Point(12, 255);
             this.dgvRes.Name = "dgvRes";
             this.dgvRes.ReadOnly = true;
             this.dgvRes.Size = new System.Drawing.Size(392, 102);
@@ -468,7 +475,7 @@
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(352, 195);
+            this.btnBuscar.Location = new System.Drawing.Point(352, 222);
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(52, 23);
             this.btnBuscar.TabIndex = 21;
@@ -501,22 +508,48 @@
             this.btnBd.UseVisualStyleBackColor = true;
             this.btnBd.Click += new System.EventHandler(this.btnBd_Click);
             // 
-            // btnFiltros
+            // btnApFiltroS
             // 
-            this.btnFiltros.Location = new System.Drawing.Point(356, 50);
-            this.btnFiltros.Name = "btnFiltros";
-            this.btnFiltros.Size = new System.Drawing.Size(49, 23);
-            this.btnFiltros.TabIndex = 24;
-            this.btnFiltros.Text = "Aplicar";
-            this.btnFiltros.UseVisualStyleBackColor = true;
+            this.btnApFiltroS.Location = new System.Drawing.Point(118, 8);
+            this.btnApFiltroS.Name = "btnApFiltroS";
+            this.btnApFiltroS.Size = new System.Drawing.Size(49, 23);
+            this.btnApFiltroS.TabIndex = 24;
+            this.btnApFiltroS.Text = "Aplicar";
+            this.btnApFiltroS.UseVisualStyleBackColor = true;
+            // 
+            // btnNingunoS
+            // 
+            this.btnNingunoS.Location = new System.Drawing.Point(95, 137);
+            this.btnNingunoS.Name = "btnNingunoS";
+            this.btnNingunoS.Size = new System.Drawing.Size(73, 23);
+            this.btnNingunoS.TabIndex = 25;
+            this.btnNingunoS.Text = "Ninguno";
+            this.btnNingunoS.UseVisualStyleBackColor = true;
+            // 
+            // btnNingunoI
+            // 
+            this.btnNingunoI.Location = new System.Drawing.Point(77, 135);
+            this.btnNingunoI.Name = "btnNingunoI";
+            this.btnNingunoI.Size = new System.Drawing.Size(63, 23);
+            this.btnNingunoI.TabIndex = 26;
+            this.btnNingunoI.Text = "Ninguno";
+            this.btnNingunoI.UseVisualStyleBackColor = true;
+            // 
+            // btnApFiltroI
+            // 
+            this.btnApFiltroI.Location = new System.Drawing.Point(123, 10);
+            this.btnApFiltroI.Name = "btnApFiltroI";
+            this.btnApFiltroI.Size = new System.Drawing.Size(49, 23);
+            this.btnApFiltroI.TabIndex = 26;
+            this.btnApFiltroI.Text = "Aplicar";
+            this.btnApFiltroI.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(420, 348);
-            this.Controls.Add(this.btnFiltros);
+            this.ClientSize = new System.Drawing.Size(420, 369);
             this.Controls.Add(this.btnBd);
             this.Controls.Add(this.txtBd);
             this.Controls.Add(this.lblBd);
@@ -591,7 +624,10 @@
         private System.Windows.Forms.Label lblBd;
         private System.Windows.Forms.TextBox txtBd;
         private System.Windows.Forms.Button btnBd;
-        private System.Windows.Forms.Button btnFiltros;
+        private System.Windows.Forms.Button btnApFiltroS;
+        private System.Windows.Forms.Button btnNingunoI;
+        private System.Windows.Forms.Button btnNingunoS;
+        private System.Windows.Forms.Button btnApFiltroI;
     }
 }
 
